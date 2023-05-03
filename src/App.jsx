7a5db8b7734fb1f7b0ca57e88673f46c61553485
david-carmigo <dlmarshall3@protonmail.com>
2023-05-02 22:44:32 -0500

    removed about

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,12 @@ import "./App.css";
 import Layout from "./pages/layout";
 import HoursStorage from "./contexts/HoursContext";
 import CalculateStorage from "./contexts/CalculateContext";
-import AboutStorage from "./contexts/AboutContext";
 
 function App() {
   return (
     <HoursStorage>
       <CalculateStorage>
-        <AboutStorage>
-          <Layout />
-        </AboutStorage>
+        <Layout />
       </CalculateStorage>
     </HoursStorage>
   );
--- src/pages/layout.jsx
+++ src/pages/layout.jsx
@@ -2,34 +2,19 @@ import { GlassPane } from "../components/GlassPane";
 import { useCalculateContext } from "../contexts/CalculateContext";
 import { Results } from "../components/Results";
 import { Woah } from "../components/Woah";
-import { About } from "../components/About";
 import { OptionsLayout } from "../components/OptionsLayout";
-import { Button } from "../components/Button";
-import { useAboutContext } from "../contexts/AboutContext";
 
 export default function ApplicationLayout() {
   const { calculateFlag, woahFlag } = useCalculateContext();
-  const { aboutFlag, setAboutFlag } = useAboutContext();
-
-  function onAboutClick() {
-    setAboutFlag(!aboutFlag);
-  }
 
   return (
     <div className="h-screen w-screen p-6 min-w-fit min-h-fit">
       <GlassPane className="flex h-full justify-center text-clip">
         <div className="p-4 text-center">
           <h1 className="text-4xl md:text-5xl mt-10">Screentime Calculator</h1>
-          {!calculateFlag && !woahFlag && !aboutFlag && (
+          {!calculateFlag && !woahFlag && (
             <div>
               <OptionsLayout />
-              <Button text="About the application" buttonClick={onAboutClick} />
-            </div>
-          )}
-
-          {aboutFlag && (
-            <div>
-              <About />
             </div>
           )}
 
